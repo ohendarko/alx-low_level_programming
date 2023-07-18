184328a0f9f6b0e25697953108c014eeda8017a6
ohendarko <ohendarko@gmail.com>
2023-07-18 23:08:24 +0000

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,21 +1,26 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* _abs - Shows 1 if the input is a lowercase character.
+* print_last_digit - Shows 1 if the input is a lowercase character.
 *Another cases, shows 0
 *
 * @i:  Variable is character in ASCII code
 *
 *Return: 1 for lowercase character. 0 for the rest.
 */
-int _abs(int i)
+int print_last_digit(int i)
 {
-	if (i >= 0)
+	int l;
+
+	l = i % 10;
+	if (l < 0)
 	{
-		return (1);
+		_putchar(-l + 48);
+		return (-l);
 	}
 	else
 	{
-		return (i * -1);
+		_putchar(l + 48);
+		return (l);
 	}
 }
