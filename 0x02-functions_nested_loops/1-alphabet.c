2f341ea3efd38a4d9628ab5c981cf1cd49f57b0b
ohendarko <ohendarko@gmail.com>
2023-07-18 21:15:23 +0000

    lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,10 +9,10 @@ void print_alphabet(void)
 {
 	int i = 97;
 
-        while (i < 123)
-        {
-                putchar (i);
-                        i++;
-        }
-        putchar('\n');
+	while (i < 123)
+	{
+	putchar (i);
+		i++;
+	}
+	putchar('\n');
 }
