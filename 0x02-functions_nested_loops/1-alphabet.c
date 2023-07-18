483be1e62627dc4cb59c9a3b76cbca60fef629cc
ohendarko <ohendarko@gmail.com>
2023-07-18 21:11:53 +0000

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_alphabet - Prints the alphabet in lowercase.
+*
+*Return: Always 0 (success)
+*/
+void print_alphabet(void)
+{
+	int i = 97;
+
+        while (i < 123)
+        {
+                putchar (i);
+                        i++;
+        }
+        putchar('\n');
+}
