bcf2302c98401992c3643f3e4ae0b25a90d740c3
ohendarko <ohendarko@gmail.com>
2023-07-19 00:34:13 +0000

    9x

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* add - Shows 1 if the input is a lowercase character.
+*Another cases, shows 0
+*
+* @n:  Variable is character in ASCII code
+*
+*Return: 1 for lowercase character. 0 for the rest.
+*/
+int add(int, int)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*times_table - Prints the alphabet in lowercase 10 times.
+*
+*Return: Always 0 (success)
+*/
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; 1++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			result = i * j
+			if (j == 0)
+			{
+				printf("%d, ", result);
+			}
+			else
+			{
+				printf("%2d, ", result);
+				if (j != 9)
+				{
+					printf(", ");
+				}
+			}
+			printf("%2d, ", result);
+		}
+		printf("\n");
+	}
+}
