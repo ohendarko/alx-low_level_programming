f9546b1451b49fc22865619aa0ad03a574661e07
ohendarko <ohendarko@gmail.com>
2023-07-20 04:39:00 +0000

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_square - product of
+* @size: variable
+* Return: void
+*/
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i,  j;
+
+		for (i = 1  ; i <= size ; i++)
+		{
+			for (j = 1 ; j <= size ; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
