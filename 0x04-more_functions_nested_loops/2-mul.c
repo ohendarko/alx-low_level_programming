5ac94fdd7bf03686a82e203be4c46635749d9c9d
ohendarko <ohendarko@gmail.com>
2023-07-20 01:39:46 +0000

    multip

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - product of
+* c: variable that is checked
+* @a: multiplied with b
+* @b: multiplied with b
+* Return: returns c
+*/
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
