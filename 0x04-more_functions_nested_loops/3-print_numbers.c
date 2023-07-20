b0beae381fa6c8f89086f845bd2121c42aad8fd1
ohendarko <ohendarko@gmail.com>
2023-07-20 02:00:25 +0000

    speak

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_numbers - product of
+* * Return: 1 if success and 0 if fail
+*/
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	} _putchar('\n');
+}
