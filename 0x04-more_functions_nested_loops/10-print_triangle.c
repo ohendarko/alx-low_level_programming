383bbf89357dec8297bc5c2f4d5fd12979033131
ohendarko <ohendarko@gmail.com>
2023-07-21 00:40:35 +0000

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,11 +17,14 @@ void print_triangle(int size)
 
 		for (i = 1 ; i <= size ; i++)
 		{
-			for (j = 19 ; j >= size ; j--)
+			for (j = i ; j < size ; j++)
 			{
 				_putchar(' ');
 			}
-			_putchar('#');
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
 			_putchar('\n');
 		}
 	}
