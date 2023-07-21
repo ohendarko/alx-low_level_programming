d62a48af5f130f2de7be446b0f1e56a19503230b
ohendarko <ohendarko@gmail.com>
2023-07-21 00:06:41 +0000

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,26 +6,29 @@
 */
 int main(void)
 {
-	int N = 100;
 	int i;
 
-	for (i = 1 ; i <= N ; i++)
+	for (i = 1 ; i <= 100 ; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf(" FizzBuzz");
+			printf("%s", "FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("%s", "Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("%s", "Buzz");
 		}
 		else
 		{
 			printf(" %d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
