a730e21df0d03f7a8d1f2cef34f91b973593a396
ohendarko <ohendarko@gmail.com>
2023-07-31 21:16:27 +0000

    to

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* set_string - betty no dey spy here
+* @s: betty no dey spy here
+* @to: betty no dey spy here
+* Return: whatever
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
