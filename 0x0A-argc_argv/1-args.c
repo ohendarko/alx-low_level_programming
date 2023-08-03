6e7860681b03c08690b16229f07d054b3485486b
ohendarko <ohendarko@gmail.com>
2023-08-03 18:54:37 +0000

    fingers crossed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - start here
+* @argc: argument count
+* @argv: argument vector
+* Return: whatever
+*/
+int main(int argc , char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
