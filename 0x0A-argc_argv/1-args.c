9a4251281aedbe34f50e3ec6f9cbb5ce0541fe60
ohendarko <ohendarko@gmail.com>
2023-08-03 18:59:48 +0000

    commiting

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,9 @@
 * @argv: argument vector
 * Return: whatever
 */
-int main(int argc , char *argv[]__attribute__((unused)))
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc);
+	(void) argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
