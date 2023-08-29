a1b47798ee32c4bb5993fd5a6ed60d5c80ac4e89
ohendarko <ohendarko@gmail.com>
2023-08-29 12:04:38 +0000

    submit

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* print_listint_safe - call to print elements
+* @head: set to null
+* Return: number of nodes
+*/
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+	const listint_t *slow, *fast;
+
+	slow = head;
+	fast = head;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			printf("-> [%p] %d\n", (void *)slow, slow->n);
+			return (count);
+		}
+
+		count++;
+	}
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		count++;
+	}
+	return (count);
+}
