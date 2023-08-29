9c2c66c6fef5d210973a2a231dd4a893885405c7
ohendarko <ohendarko@gmail.com>
2023-08-29 12:32:06 +0000

    submit

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,30 +9,30 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *slow, *fast;
+	const listint_t *current = head;
+	const listint_t *slow = head;
+	const listint_t *fast = head;
 
-	slow = head;
-	fast = head;
-
-	while (fast != NULL && fast->next != NULL)
+	while (current != NULL)
 	{
-		printf("[%p] %d\n", (void *)slow, slow->n);
-		slow = slow->next;
-		fast = fast->next->next;
+		printf("[%p] %d\n", (void *)current, current->n);
+		current = current->next;
+		count++;
 
-		if (slow == fast)
+		if (fast != NULL && fast->next != NULL)
 		{
-			printf("-> [%p] %d\n", (void *)slow, slow->n);
-			return (count);
-		}
+			slow = slow->next;
+			fast = fast->next->next;
 
-		count++;
+			if (slow == fast)
+			{
+				printf("-> [%p] %d\n", (void *)slow, slow->n);
+				printf("Loop detected!\n");
+				return (count);
+			}
+		}
 	}
-	while (head != NULL)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		count++;
-	}
+
 	return (count);
+
 }
