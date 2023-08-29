178ed496611d79743a8b4c57078a2df439022ef0
ohendarko <ohendarko@gmail.com>
2023-08-29 12:40:15 +0000

    submit

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -24,9 +24,22 @@ size_t print_listint_safe(const listint_t *head)
 
 		if (slow == fast)
 		{
-			printf("-> [%p] %d\n", (void *)slow, slow->n);
+			printf("[%p] %d\n", (void *)slow, slow->n);
 			break;
 		}
+	}
+
+	if (slow == fast)
+	{
+		slow = head;
+		while (slow != fast)
+		{
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			slow = slow->next;
+			fast = fast->next;
+			count++;
+		}
+		printf("-> [%p] %d\n", (void *)slow, slow->n);
 	}
 
 	return (count);
