18000bb4754291955fe8f0a4b21d0a41b6b67b3f
ohendarko <ohendarko@gmail.com>
2023-08-29 10:38:20 +0000

    submit

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* delete_nodeint_at_index - call to print elements
+* @head: address
+* @index: index of list
+* n: not at this time
+* Return: new node
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tracknow = *head;
+	listint_t *trackprev = NULL;
+	unsigned int count = 0;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(tracknow);
+		return (1);
+	}
+	while (count < index - 1 && tracknow != NULL)
+	{
+		trackprev = tracknow;
+		tracknow = tracknow->next;
+		count++;
+	}
+	if (count < index)
+	{
+		return (0);
+	}
+
+	trackprev->next = tracknow->next;
+
+	free(tracknow);
+
+	return (1);
+}
