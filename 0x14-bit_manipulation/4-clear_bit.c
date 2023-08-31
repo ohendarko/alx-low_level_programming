8b4c615630b5c9f7f12026cd9369217761d71955
ohendarko <ohendarko@gmail.com>
2023-08-31 20:53:38 +0000

    submit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* get_bit - function name
+* @n: parameter
+* @index: parameter
+* Return: as usual
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{	
+	unsigned long int alpha = 1UL << index;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	*n &= ~alpha;
+
+	return (1);
+}
