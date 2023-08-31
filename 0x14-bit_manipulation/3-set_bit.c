b9d864fc78c6a41be9d0c4f1dedadddfdc0aeef0
ohendarko <ohendarko@gmail.com>
2023-08-31 20:59:47 +0000

    submit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,11 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 /**
-* get_bit - function name
+* set_bit - function name
 * @n: parameter
 * @index: parameter
 * Return: as usual
 */
 int set_bit(unsigned long int *n, unsigned int index)
-{	
+{
+	unsigned long int omega = 1UL << index;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	*n |= omega;
+	return (1);
 }
