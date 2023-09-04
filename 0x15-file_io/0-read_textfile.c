9bc00ed768adae8f35ba9cd318e71567812e92e8
ohendarko <ohendarko@gmail.com>
2023-09-04 22:27:23 +0000

    submit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* read_textfile - function name
+* @filename: parameter
+* @letters: parameter
+* Return: what is expected
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *file;
+	ssize_t n;
+	char buffer[1024];
+
+	file = fopen(filename, "r");
+	if (file == NULL)
+	{
+		return (0);
+	}
+
+	n = fread(buffer, sizeof(char), letters, file);
+
+	if (n == 0 && !feof(file))
+	{
+		fclose(file);
+		return (-1);
+	}
+
+	fwrite(buffer, sizeof(char), n, stdout);
+	fclose(file);
+
+	return (n);
+}
