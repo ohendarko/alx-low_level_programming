124dc270522bc283ed635a484ce36abf2a479a68
ohendarko <ohendarko@gmail.com>
2023-10-12 18:09:01 +0000

    submit

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,17 @@
 #include "lists.h"
+/**
+* dlistint_len - print all list elements
+* @h: beats me
+* Return: number of nodes
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
