da0bd213f5b9aad6154940871878569b98475bbb
ohendarko <ohendarko@gmail.com>
2024-02-06 22:56:19 +0000

    submit

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -6,26 +6,29 @@
 * @array: pointer to the first element of the array to search in
 * @value:  value to search for
 * @size: number of elements in array
-* Return: the index where value is located
+* Return: the first index where value is located
 */
 int jump_search(int *array, size_t size, int value)
 {
 	int jump_step = (int)sqrt(size);
 	unsigned int prev = 0, i;
-	unsigned int curr = jump_step;
 
-	if (array == NULL || size == 0)
+	if (array == NULL)
 		return (-1);
-
-	while (curr < size && array[curr] < value)
+	while (array[prev] < value)
 	{
-		printf("Value checked array[%d]: %d\n", curr, array[curr]);
-		prev = curr;
-		curr += jump_step;
+		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+		if (prev + jump_step >= size)
+			break;
+		if (array[prev + jump_step] >= value)
+			break;
+		prev += jump_step;
 	}
-	for (i = prev; i < curr && i < size; i++)
+	printf("Value found between indexes [%d] and [%d]\n", prev, prev + jump_step);
+
+	for (i = prev; i < size && i <= prev + jump_step; i++)
 	{
-		printf("Value checked array[%d]: %d\n", curr, array[curr]);
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
