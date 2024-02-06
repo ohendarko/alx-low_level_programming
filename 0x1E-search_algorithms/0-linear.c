fc88aebadbe2aaf93f980a2d3601f76f8dffe949
ohendarko <ohendarko@gmail.com>
2024-02-06 21:34:47 +0000

    submit

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,12 +9,13 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
-	long unsigned int i;
+	unsigned int i;
 
 	if (array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
